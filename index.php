1ff56f4b7bbf729685fa92f7dbe3540ea2b3d708
Felix Biego <fbiego.fb@gmail.com>
2021-09-23 21:04:33 +0300

    Update index.php

--- index.php
+++ index.php
@@ -124,7 +124,7 @@ html, body, h1, h2, h3, h4, h5 {font-family: "Open Sans", sans-serif}
 		</p>
 		<div class="w3-row-padding w3-margin-bottom">
     <div class="w3-quarter">
-      <div class="w3-container w3-red w3-padding-16">
+      <div class="w3-container w3-green w3-padding-16">
         <div class="w3-left"><i class="fa fa-comment w3-xxxlarge"></i></div>
         <div class="w3-right">
           <h3><?php echo count($following); ?></h3>
@@ -144,7 +144,7 @@ html, body, h1, h2, h3, h4, h5 {font-family: "Open Sans", sans-serif}
       </div>
     </div>
     <div class="w3-quarter">
-      <div class="w3-container w3-teal w3-padding-16">
+      <div class="w3-container w3-purple w3-padding-16">
         <div class="w3-left"><i class="fa fa-share-alt w3-xxxlarge"></i></div>
         <div class="w3-right">
           <h3><?php echo count($dif1); ?></h3>
